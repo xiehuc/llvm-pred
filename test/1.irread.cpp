d749edfb1bc427927c839eba8badc23ddd157971
xiehuc <xiehuc@gmail.com>
2013-10-01 21:49:13 +0800

    add inst counter
    Signed-off-by: xiehuc <xiehuc@gmail.com>

--- test/1.irread.cpp
+++ test/1.irread.cpp
@@ -2,10 +2,14 @@
 #include <llvm/IR/LLVMContext.h>
 #include <llvm/Support/SourceMgr.h>
 #include <llvm/IRReader/IRReader.h>
-#include <stdio.h>
+#include <llvm/Support/InstIterator.h>
+#include <iostream>
+#include <map>
 
 using namespace llvm;
 using namespace std;
+
+static map<unsigned,int> inst_counter;
 
 int main(int argc,char** argv)
 {
@@ -19,5 +23,16 @@ int main(int argc,char** argv)
     SMDiagnostic err;
     Module* mod;
     mod = ParseIRFile(argv[1], err, cnt);
+
+    for (const auto& func : *mod){
+        for(const_inst_iterator inst=inst_begin(func),
+                inst_e=inst_end(func);inst!=inst_e;inst++){
+            inst_counter[inst->getOpcode()]++;
+        }
+    }
+
+    for(const auto& pair : inst_counter){
+        cout<<Instruction::getOpcodeName(pair.first)<<":"<<pair.second<<endl;
+    }
 
 }
