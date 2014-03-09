22f6d518c7211c8279550e198969beb7df7dec6b
xiehuc <xiehuc@gmail.com>
2014-03-09 11:31:57 +0800

    optimize drawline for variable doesn't change
    使用绘制起始点和终止点的直线来代替绘制所有的点,减小cpu和内存的压力
    
    Signed-off-by: xiehuc <xiehuc@gmail.com>

--- scripts/drawline.py
+++ scripts/drawline.py
@@ -3,6 +3,7 @@
 import argparse
 from os import mkdir,path
 from matplotlib import pyplot as plt
+import re
 
 def parse_one_line(line):
     ar=line.split('\t')
@@ -10,9 +11,21 @@ def parse_one_line(line):
     end=ar[0].find(')')
     count=int(ar[0][9:end])
     #remove last \n
-    content=tuple(map(int,ar[1].split(',')[:-1]))
-    if not isConstant:
-        assert(len(content)==count)
+    compact=tuple(ar[1].split(',')[:-1])
+    content=[[],[]] #x and y
+    x=0
+    for item in compact:
+        c = re.findall('(\d+)<repeat (\d+) times>',item)
+        if c:
+            content[0]+=[x]
+            content[1]+=[int(c[0][0])]
+            x+=int(c[0][1])
+            content[0]+=[x-1]
+            content[1]+=[int(c[0][0])]
+        else:
+            content[0]+=[x]
+            content[1]+=[int(item)]
+            x+=1
     return (isConstant,count,content)
 
 def main():
@@ -30,7 +43,10 @@ def main():
     for line in f:
         data = parse_one_line(line)
         print('#',idx)
-        plt.plot(data[2],'.')
+        if len(data[2][0])==1:
+            plt.plot(data[2][0],data[2][1],'.')
+        else:
+            plt.plot(data[2][0],data[2][1],'-')
         plt.savefig(path.join(outdir,str(idx)+".png"))
         plt.close()
         idx+=1
