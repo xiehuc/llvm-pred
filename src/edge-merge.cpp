48b14c12f275e124e5cfe6cd2ca98c8e0743fce0
xiehuc <xiehuc@gmail.com>
2014-03-15 22:32:36 +0800

    modify indent, retab file
    Signed-off-by: xiehuc <xiehuc@gmail.com>

--- src/edge-merge.cpp
+++ src/edge-merge.cpp
@@ -8,13 +8,13 @@
 #include <regex>
 
 /**
-  *以前使用llvmprof处理二进制文件后，得到的可读文件分为两部分，
-  *上半部分是TOP 20的函数，下半部分是TOP 20的基本块。都是用##标志。
-  *使用llvmpred处理二进制文件后，得到的可读文件只有一部分，TOP 20
-  *的基本块，也是用##标志。
-  *在验证本程序的时候，使用的是以前的文件，里面是两部分，所以PART_NUM
-  *定义为2。如果是处理现在的文件，里面只有一部分，就要将PART_NUM定义为1，
-  *不然会出现死循环。
+ *以前使用llvmprof处理二进制文件后，得到的可读文件分为两部分，
+ *上半部分是TOP 20的函数，下半部分是TOP 20的基本块。都是用##标志。
+ *使用llvmpred处理二进制文件后，得到的可读文件只有一部分，TOP 20
+ *的基本块，也是用##标志。
+ *在验证本程序的时候，使用的是以前的文件，里面是两部分，所以PART_NUM
+ *定义为2。如果是处理现在的文件，里面只有一部分，就要将PART_NUM定义为1，
+ *不然会出现死循环。
  **/
 #define PART_NUM 2
 #define MAX_BUF_SIZE 200
@@ -35,7 +35,7 @@ int read_filelist(char * dir_name,vector<string> &filelist)
 {
 	struct dirent * file_info = NULL;
 	string cur_dir = dir_name, pat_pre("llvmprof.out."),pat_suf(".output");
-        DIR * dir = NULL;
+	DIR * dir = NULL;
 	string file_name;
 
 	if(cur_dir.at(cur_dir.length()-1) != '/')  
@@ -62,12 +62,12 @@ unsigned long long  parse_buf(string & buf, vector<DType> &freq_count)
 	char name_pre[MAX_BUF_SIZE], name_suf[MAX_BUF_SIZE];
 	long double freq_temp,sum_freq_temp;
 	unsigned long long freqence,sum_freq;
-	
+
 	sscanf(buf.c_str(),"%*d%*c%*f%*c%Lf%*c%Lf%s%[0-9a-zA-Z<>_- .()]",&freq_temp,&sum_freq_temp,name_pre,name_suf);
 	freqence = freq_temp;
 	sum_freq = sum_freq_temp;
 	name = string(name_pre)+string(name_suf);
-        
+
 	for(vector<DType>::iterator I=freq_count.begin(),E=freq_count.end();I!=E;++I)
 	{
 		if(name == I->func_id)
@@ -84,7 +84,7 @@ unsigned long long extract_info(fstream &infile,vector<DType> &freq_count)
 {
 	int count = 0;
 	unsigned long long sum_per_file;
-        string buf;
+	string buf;
 
 	while(count<PART_NUM)
 	{
@@ -104,36 +104,36 @@ int main(int argc,char *argv[])
 {
 	vector<string> filelist;
 	vector<DType> freq_count;
-        string file_path,out_file_name;
-        fstream infile,outfile;
+	string file_path,out_file_name;
+	fstream infile,outfile;
 	stringstream ss;
 	double out_num;
-        unsigned long long sum_freq = 0;
+	unsigned long long sum_freq = 0;
 
 	if(argc != 2)
 	{
 		cout << "Usage: "<<argv[0]<<" [directory-where-the-files-exist]" << endl
- 		     << "Like: "<<argv[0]<<" /home/lgz/Document/" << endl;
+			<< "Like: "<<argv[0]<<" /home/lgz/Document/" << endl;
 		return -1;
 	}
 	if(access(argv[1],R_OK)!=0)
 	{
 		cout << "Sorry,you can not read the director,check to see if the"
-                     << " file exists or you have the right to read it" << endl;
-                return -1;
-        }
+			<< " file exists or you have the right to read it" << endl;
+		return -1;
+	}
 
-        read_filelist(argv[1],filelist);
+	read_filelist(argv[1],filelist);
 	ss << "collect" << filelist.size() << ".txt";
 	ss >> out_file_name;
 	outfile.open(out_file_name,fstream::out);
-        if(!outfile)
-        {
+	if(!outfile)
+	{
 		cerr << "Error:Can't open the file " << out_file_name << endl;
 		return -1;
 	}
 
-        while(!filelist.empty())
+	while(!filelist.empty())
 	{
 		file_path = filelist.back();
 		filelist.pop_back();
@@ -150,7 +150,7 @@ int main(int argc,char *argv[])
 	for(vector<DType>::iterator it = freq_count.begin();it!=freq_count.end();it++)
 	{
 		outfile  << (it->freqence*1.0/sum_freq)*100.0 << "%   \t" << it->freqence 
-                         << "/" << sum_freq << "\t\t" << it->func_id << endl;
+			<< "/" << sum_freq << "\t\t" << it->func_id << endl;
 	}
 	return 0;
 }
