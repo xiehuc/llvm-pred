6cc0803d4e5876825f59b9ba41b015dbfc323b99
xiehuc <xiehuc@gmail.com>
2014-05-24 14:56:30 +0800

    add dirdiff.sh
    Signed-off-by: xiehuc <xiehuc@gmail.com>

---
+++ scripts/dirdiff.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# a script to help compare two directories llvmprof.out file
+
+[ $# -ne 2 ] && echo "useage: $0 dir1 dir2";exit
+
+lhs=(`ls -1 --color=never $1`)
+rhs=(`ls -1 --color=never $2`)
+
+if [ ${#lhs[@]} -ne ${#rhs[@]} ]
+then
+   echo "$1 file numbers not match with $2"
+   exit
+fi
+
+sz=${#lhs[@]}
+echo "$1 <---> $2"
+for (( i=0 ; i<$sz ; i++ ))
+do
+   echo "${lhs[$i]} <---> ${rhs[$i]} :"
+   llvm-prof -diff $1/${lhs[$i]} $2/${rhs[$i]}
+done
+
