a850d89c7a72bf4d3b85a38a0531ba351121d75b
xiehuc <xiehuc@gmail.com>
2014-10-15 10:59:57 +0800

    freqavg.py recognise arg order
    Signed-off-by: xiehuc <xiehuc@gmail.com>

--- scripts/freqavg.py
+++ scripts/freqavg.py
@@ -5,8 +5,8 @@ import argparse
 import re
 
 def perror(*args, **keys):
-    import sys.stderr
-    print(args, keys, file=sys.stderr)
+    from sys import stderr
+    print(file=stderr, *args, **keys)
     exit(-1)
 
 def calc_average(v2e_map, e_map):
@@ -18,9 +18,9 @@ def calc_average(v2e_map, e_map):
     for k in v2e_map.keys() & e_map.keys():
         predict = int(v2e_map[k])
         real = int(e_map[k])
-        diff = abs(predict - real)/real
+        diff = abs(predict - real)/min(real, predict)
         if diff > 2:
-            print("predicted:", predict, "\treal:", real, "\t rate:", diff, "\t", k)
+            print("predicted:%7d real:%7d rate:%.2f\t%s" % (predict, real, diff, k))
         else:
             part += diff
             part_n += 1
@@ -34,12 +34,11 @@ def calc_average(v2e_map, e_map):
 
 def read_profiling(filename):
     handle = open(filename, 'r')
-    anchor_ready = False # found begin position in text
+    anchor_ready = cmd_ready = False # found begin position in text
     data = {}
     pattern = re.compile(r'(\d+)/[0-9.e+]+\s+(.+)')
+    is_v2e = True 
     for line in handle:
-        if line.startswith('Sorted executed basic blocks'):
-            anchor_ready = True
         if anchor_ready:
             m = pattern.search(line)
             if m == None:
@@ -47,11 +46,19 @@ def read_profiling(filename):
             # freq == m.group(1)
             # name == m.group(2)
             data[m.group(2)] = m.group(1)
+        if cmd_ready: 
+            print(line)
+            is_v2e = line.strip('\r\n') == ""
+            cmd_ready = False
+        if line.startswith('Sorted executed basic blocks'):
+            anchor_ready = True
+        if line.startswith('LLVM profiling output for execution'):
+            cmd_ready = True
     handle.close()
 
     if not anchor_ready:
         return None
-    return data
+    return data, is_v2e
 
 def main():
     parser = argparse.ArgumentParser(description = 'caculate average compare between value-to-edge and edge profiling')
@@ -59,10 +66,15 @@ def main():
     parser.add_argument('edge', type=str)
     args = parser.parse_args()
 
-    v2e_map = read_profiling(args.value_to_edge)
-    e_map = read_profiling(args.edge)
+    v2e_map, yes_v2e = read_profiling(args.value_to_edge)
+    e_map, no_v2e = read_profiling(args.edge)
+    print(yes_v2e, no_v2e)
     if e_map == None or v2e_map == None:
         perror('file format unmatch, please use ``llvm-prof -list-all`` to generate output')
+    if yes_v2e == no_v2e:
+        perror('arg order unmatch, first arg should be value_to_edge, second should be edge')
+    if yes_v2e == False: # swap order
+        v2e_map, e_map = e_map, v2e_map
     calc_average(v2e_map, e_map)
 
 main()
